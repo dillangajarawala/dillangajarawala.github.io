0353f62ef67fba07d5c97ac10137043465b09a96
Dillan Gajarawala <dillan.gajarawala@gmail.com>
2019-07-09 22:41:12 -0700

    added cool scrolly button

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,6 @@ function scrollToTop() {
     const c = document.documentElement.scrollTop || document.body.scrollTop;
     if (c > 0) {
       window.requestAnimationFrame(scrollToTop);
-      window.scrollTo(0, c - c / 16);
+      window.scrollTo(0, c - c / 20);
     }
 };
