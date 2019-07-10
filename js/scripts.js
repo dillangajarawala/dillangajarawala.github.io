5b71f3cea1b0da61015a6de613912264225931f1
Dillan Gajarawala <dillan.gajarawala@gmail.com>
2019-07-09 20:54:13 -0700

    added smooth scrolling function

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,11 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.parallax');
     var instances = M.Parallax.init(elems, {responsiveThreshold: 0});
   });
+
+function scrollToTop() {
+    const c = document.documentElement.scrollTop || document.body.scrollTop;
+    if (c > 0) {
+      window.requestAnimationFrame(scrollToTop);
+      window.scrollTo(0, c - c / 16);
+    }
+};
