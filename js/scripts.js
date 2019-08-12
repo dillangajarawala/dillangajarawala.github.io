137bb48e4ae89231f9d02f91a85a1b68865df771
Dillan Gajarawala <dillan.gajarawala@gmail.com>
2019-08-12 12:16:50 -0400

    added working sidenav

--- js/scripts.js
+++ js/scripts.js
@@ -40,3 +40,8 @@ document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.modal');
   var instances = M.Modal.init(elems, {preventScrolling: true});
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.sidenav');
+  var instances = M.Sidenav.init(elems, {});
+});
